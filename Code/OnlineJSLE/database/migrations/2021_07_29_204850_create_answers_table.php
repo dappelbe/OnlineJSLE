6b847e8a5c2e8e6fff7869a5202159211c0d0ea3
dappelbe <15815803+dappelbe@users.noreply.github.com>
2021-07-29 22:18:44 +0100

    Initial setup - setup docker-compose.yml - downloaded Laravel and setup Livewire

---
+++ Code/OnlineJSLE/database/migrations/2021_07_29_204850_create_answers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('ppt_id');
+            $table->integer('form_id');
+            $table->integer('question_id');
+            $table->text('answer');
+            $table->integer('visitrepeat');
+            $table->integer('formrepeat');
+            $table->integer('questionrepeat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# OnlineJSLE
-An online data collection system for the JSLE project.
+
+![PHP](https://img.shields.io/badge/php-%3E%3D7.4-blue)
+![GitHub last commit](https://img.shields.io/github/last-commit/dappelbe/OnlineJSLE)
+![GitHub issues](https://img.shields.io/github/issues-raw/dappelbe/OnlineJSLE)
+![GitHub issues](https://img.shields.io/github/issues/dappelbe/OnlineJSLE)
+## Overview
+## Latest Stable Release
+## Changes
+## Dependencies
+## Installation
+## Basic Usage
+## Contribution
